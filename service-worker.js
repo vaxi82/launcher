0fd839e71560a215c4af696415b2e665a75fd6f6
vaxi82 <orion_viper@hotmail.com>
2026-03-19 23:04:03 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,9 @@ self.addEventListener("install", e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/icon.png"
+        "/launcher/",
+        "/launcher/index.html",
+        "/launcher/icon.png"
       ]);
     })
   );
